9490ea225c9fa5e0efca9ed74d8a582537e752a9
Jolas Casallas <casallasjolas28@gmail.com>
2024-06-25 15:16:56 +0800

    feat(layout): add layout for header
    1. Create a header
    2. Create a layout for all aspect
    3. Additional tailwind configuration

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,15 @@
+<header class="py-6 px-4 bg-customPurple sticky top-0 z-50">
+    <div class="container mx-auto flex justify-between items-center">
+        <div class="flex items-center">
+            <h1 class="text-xl font-bold">Jolas Casallas</h1>
+            <span class="ml-2 text-lg">casallasjolas28@gmail.com</span>
+        </div>
+        <nav class="flex gap-4">
+            <a href="#" class="hover:text-purple-500">Project</a>
+            <a href="#" class="hover:text-purple-500">Works</a>
+            <a href="#" class="hover:text-purple-500">Resume</a>
+            <a href="#" class="hover:text-purple-500">Skills</a>
+            <a href="#" class="hover:text-purple-500">Contact</a>
+        </nav>
+    </div>
+</header>
